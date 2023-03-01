313e2e14b05a34e539ad4306a062aabb93f96e7f
Le Tien <tienle21032001@gmail.com>
2023-03-02 00:59:45 +0700

    Test Layout Home Page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,8 +2,12 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:second_hand_app/pages/cart.dart';
+import 'package:second_hand_app/utils/app_styles.dart';
+import 'package:second_hand_app/utils/size_config.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,13 +17,297 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomeState extends State<HomePage> {
+
+  List<String> categories = [
+    "All Items",
+    "Dress",
+    "Hat",
+    "Watch",
+  ];
+
+  List<String> icons = [
+    'all_items_icon',
+    'dress_icon',
+    'hat_icon',
+    'watch_icon',
+  ];
+
+  List<String> images = [
+    'image-01.png',
+    'image-02.png',
+    'image-03.png',
+    'image-04.png',
+    'image-05.jpg',
+    'image-06.jpg',
+    'image-07.jpg',
+    'image-08.jpg',
+  ];
+
+  int current = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(
-        child: Text('Home Screen', style: TextStyle(fontSize: 40)),
+    SizeConfig().init(context);
+
+    return SafeArea(
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kPaddingHorizontal),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Hello, Welcome 👋',
+                      style: kEncodeSansRagular.copyWith(
+                        color: kDarkBrown,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                      ),
+                    ),
+                    Text(
+                      'Albert Stefano',
+                      style: kEncodeSansBold.copyWith(
+                        color: kDarkBrown,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                      ),
+                    ),
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundColor: kGrey,
+                  backgroundImage: NetworkImage(
+                      'https://randomuser.me/api/portraits/women/90.jpg'),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: kPaddingHorizontal,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: kEncodeSansRagular.copyWith(
+                      color: kDarkGrey,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                    ),
+                    controller: TextEditingController(),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 13,
+                      ),
+                      prefixIcon: const IconTheme(
+                        data: IconThemeData(
+                          color: kDarkGrey,
+                        ),
+                        child: Icon(Icons.search),
+                      ),
+                      hintText: 'Sarch clothes...',
+                      border: kInputBorder,
+                      errorBorder: kInputBorder,
+                      disabledBorder: kInputBorder,
+                      focusedBorder: kInputBorder,
+                      focusedErrorBorder: kInputBorder,
+                      enabledBorder: kInputBorder,
+                      hintStyle: kEncodeSansRagular.copyWith(
+                        color: kDarkGrey,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Container(
+                  height: 49,
+                  width: 49,
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    color: kBlack,
+                  ),
+                  child: SvgPicture.asset(
+                    'assets/images/filter_icon.svg',
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 36,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      current = index;
+                    });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      left: index == 0 ? kPaddingHorizontal : 15,
+                      right: index == categories.length - 1
+                          ? kPaddingHorizontal
+                          : 0,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    height: 36,
+                    decoration: BoxDecoration(
+                      color: current == index ? kBrown : kWhite,
+                      borderRadius: BorderRadius.circular(8),
+                      border: current == index
+                          ? null
+                          : Border.all(
+                        color: kLightGrey,
+                        width: 1,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(current == index
+                            ? 'assets/images/${icons[index]}_selected.svg'
+                            : 'assets/images/${icons[index]}_unselected.svg'),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          categories[index],
+                          style: kEncodeSansMedium.copyWith(
+                            color: current == index ? kWhite : kDarkBrown,
+                            fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          MasonryGridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 23,
+            itemCount: images.length,
+            padding: const EdgeInsets.symmetric(
+              horizontal: kPaddingHorizontal,
+            ),
+            itemBuilder: (context, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          child: Image.asset(
+                            'assets/images/${images[index]}',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 12,
+                        top: 12,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: SvgPicture.asset(
+                            'assets/images/favorite_cloth_icon_unselected.svg',
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Modern light clothes',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansSemibold.copyWith(
+                      color: kDarkBrown,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                    ),
+                  ),
+                  Text(
+                    'Dress modern',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansRagular.copyWith(
+                      color: kGrey,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '\$212.99',
+                        style: kEncodeSansSemibold.copyWith(
+                          color: kDarkBrown,
+                          fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: kYellow,
+                            size: 16,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '5.0',
+                            style: kEncodeSansRagular.copyWith(
+                              color: kDarkBrown,
+                              fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
